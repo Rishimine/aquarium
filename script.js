97d613ff7e0b1d41e223785583525ea1756bba2a
DESKTOP-O5C829I\mspr5 <hatsune161016@gmail.com>
2022-02-18 17:14:01 +0900

    編集しました。rinaさんの編集部分コメントで区切ってます。

---
+++ script.js
@@ -0,0 +1,21 @@
+// console.log("test");
+
+$(function(){
+
+    // console.log("test");
+
+    //魚たべる
+    $(".sakana_icon").on("click",function(){
+        $(".sakana_icon").toggleClass('eat');
+
+     //さかな泳ぐ
+        $(".sakana_mure").toggleClass('oyogu');
+    })
+
+    /* -- リナさん編集 -- */
+
+})
+
+
+
+
